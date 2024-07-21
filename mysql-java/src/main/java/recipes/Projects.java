72892aafa16761e3c1d4857d97fde448c6575482
LilYang915 <yangcthao@gmail.com>
2024-07-20 21:13:38 -0500

    Add files via upload

---
+++ mysql-java/src/main/java/recipes/Projects.java
@@ -0,0 +1,19 @@
+package recipes;
+
+import recipes.dao.DbConnection;
+
+public class Projects {
+
+	public static void main(String[] args) {
+	
+		DbConnection.getConnection();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
